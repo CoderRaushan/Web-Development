d6117812af8173d44c14a458f1984880d45959c6
Raushan <raushankumar23082004@gmail.com>
2023-12-02 16:27:38 +0530

    sgpa calcultor added now

---
+++ delta/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Download PDF using PHP from HTML Link</title>
+</head>
+<body>
+<center>
+<h2 style="color: #d7385e;">Welcome To Thapa Technical</h2>
+<p><b>Click below to download PDF </b> </p>
+<a href="js.pdf" target="_thapa">Download PDF Now</a>
+</center>
+</body>
+</html>
